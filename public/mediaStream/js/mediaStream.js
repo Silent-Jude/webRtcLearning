4e63bed313ab64b3a78d0cee23aa2f0cd2fca5b7
tongzhipei <tong.zhipei@vpclub.cn>
2021-06-28 10:05:10 +0800

    测试摄像头切换

--- public/mediaStream/js/mediaStream.js
+++ public/mediaStream/js/mediaStream.js
@@ -14,22 +14,27 @@ const videoInputSelect = document.querySelector('select#videoInput')
  */
 const getUserMedia = _=> {
   const videoDeviceId = videoInputSelect.value
-  // 针对音视频的限制和设置
+  // 针对音视频的限制和设置,值改变后必须重新调用才生效。
   const constraints = {
-    video: { //关闭设置false, 具体配置用对象
-      width: 200,
+    video: { //关闭设置false, 具体配置用对象  https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints#properties_of_audio_tracks
+      width: 200, // {min: 200, ideal: 250, max: 300 }
       height: 200,
       frameRate: {min:10, ideal: 15, max: 30 }, //帧率,width，height，这些数字都有min，max，ideal
       facingMode: 'enviroment', // enviroment:后置摄像头;user:前置摄像头;left:前置左侧;right:前置右侧
+      deviceID: videoDeviceId || undefined, // 设备id，以便切换。
     },
-    audio: {
+    audio: { // https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints#properties_of_video_tracks
       volume: 1, // 音量 ，0-1
       sampleRate: 8000, //采样率
       sampleSize: 16, // 采样位深，一般是16位
       echoCancellation: true, // true,false,是否启用回音消除
       autoGainControl: true, // true, false 是否启用自动增强。
-      noiseSuppression: true,
-    }
+      noiseSuppression: true, // true,false 降噪，也可以设置具体返回，但是文档里没有范围示例，且不在范围内的数值会导致报错。
+      latency: 100.00, // 延时，长了影响即时性，短了容易造成卡顿。
+      channelCount: 1, // 声道，单声道或者双声道，ConstrainULong类型，看不懂范围。
+      // deviceID: undefined, // 设备id，以便切换。
+      // groupID: '', // 代表同一个物理设备，输入输出这些类型。
+    },
   }
   navigator.mediaDevices.getUserMedia(constraints).then(stream => {
     // videPlay.srcObject = stream
@@ -41,14 +46,11 @@ const getUserMedia = _=> {
       // 防止在新的浏览器里使用它，应为它已经不再支持了
       videPlay.src = window.URL.createObjectURL(stream);
     }
-
     // * 获取设备信息
     // * 获取到权限才可以输出label和id
     return navigator.mediaDevices.enumerateDevices()
   }).then(deviceInfos => {
     console.log('deviceInfos', deviceInfos)
-
-    
     deviceInfos.forEach(deviceInfo => {
       const option = document.createElement('option')
       option.text = deviceInfo.label
@@ -64,14 +66,15 @@ const getUserMedia = _=> {
   }).catch(handleError)
 }
 
-
-if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-  console.log('enumerateDevices is not supported! please use chrome')
-  console.log('navigator.mediaDevices:', navigator.mediaDevices)
-} else {
-  // showDevices()
-  getUserMedia()
-  // const ePromise = navigator.mediaDevices.enumerateDevices
-  // ePromise().then(gotDevices).catch()
+const startVideo = _=> {
+  if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    console.log('enumerateDevices is not supported! please use chrome')
+    console.log('navigator.mediaDevices:', navigator.mediaDevices)
+  } else {
+    getUserMedia()
+  }
 }
 
+startVideo()
+
+videoInputSelect.addEventListener('change', startVideo)
